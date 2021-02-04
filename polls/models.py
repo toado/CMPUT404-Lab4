3efd487e98a09f6d0fba69708b9f685747ed0c8b
justin <tan4@ualberta.ca>
2021-02-03 18:03:28 -0700

    Finish q4 - create models and migrate

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+# These models give Django lots of informations and the ability to do:
+# 1. Create a database schema (CREATE TABLE statements) for the app
+# 2. Create a Python database-access API for accessing Question & Choice objects
+
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    # ForeignKey tells Django that each Choice is related to a single Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
